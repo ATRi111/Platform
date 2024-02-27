40ea5b1789ba6b4cae7af8b8f622ea093f3461a7
1657125180@qq.com <1657125180@qq.com>
2024-02-27 17:27:20 +0800

    ——

--- Assets/Scripts/User/User.cs
+++ Assets/Scripts/User/User.cs
@@ -3,28 +3,28 @@ using UnityEngine;
 
 public class User : NetworkBehaviour
 {
-    private ChargingStation station;
+    //private ChargingStation station;
 
-    private void Start()
-    {
-        station = GameObject.Find(nameof(ChargingStation)).GetComponent<ChargingStation>();
-        transform.position = Camera.main.transform.position;
-    }
+    //private void Start()
+    //{
+    //    station = GameObject.Find(nameof(ChargingStation)).GetComponent<ChargingStation>();
+    //    transform.position = Camera.main.transform.position;
+    //}
 
-    private void Update()
-    {
-        if (IsClient && IsOwner)
-        {
-            if (Input.GetKeyDown(KeyCode.S))
-            {
-                TestServerRpc();
-            }
-        }
-    }
+    //private void Update()
+    //{
+    //    if (IsClient && IsOwner)
+    //    {
+    //        if (Input.GetKeyDown(KeyCode.S))
+    //        {
+    //            TestServerRpc();
+    //        }
+    //    }
+    //}
 
-    [ServerRpc]
-    private void TestServerRpc()
-    {
-        station.Use();
-    }
+    //[ServerRpc]
+    //private void TestServerRpc()
+    //{
+    //    station.Use();
+    //}
 }
