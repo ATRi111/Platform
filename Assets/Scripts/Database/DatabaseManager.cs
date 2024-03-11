79d7cdc0aa66ba0632c736ead8bf811c7c28eacf
1657125180@qq.com <1657125180@qq.com>
2024-03-11 11:02:45 +0800

    连接数据库

--- Assets/Scripts/Database/DatabaseManager.cs
+++ Assets/Scripts/Database/DatabaseManager.cs
@@ -7,6 +7,15 @@ public class DatabaseManager : NetworkBehaviour
 
     private void Start()
     {
-        
+        if(IsServer)
+        {
+            connection = new MySqlConnection(
+                "server=127.0.0.1;" +
+                "port=3306;" +
+                "user=root;" +
+                "password=123456;" +
+                "database=Platform");
+            connection.Open();
+        }
     }
 }
